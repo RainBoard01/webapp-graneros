1d05e626120b1afc33281798bb4b1a479f9917cb
RainBoard01 <amdmsi4ever@hotmail.com>
2019-09-23 15:29:34 -0300

    React-Table Implemented

--- src/components/RecordsList.js
+++ src/components/RecordsList.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { useGraphQL } from 'graphql-react';
+import { useTable } from 'react-table';
 
 import TitleBar from './TitleBar';
-import Record from './Record';
+/*import Record from './Record';*/
 import Errors from './Errors';
 
-const RecordsList = () => {
+/*const RecordsList = () => {
     const { loading, cacheValue: { data, ...errors } = {} } = useGraphQL({
         fetchOptionsOverride(options) {
-            options.url = 'http://localhost:4000/graphql'
+            options.url = 'http://192.168.0.109:4000/graphql'
         },
         operation: {
             query: `{
-                records {
-                    id
+                records(limit: 0) {
+                    dateIn
+                    person {
+                        name
+                        rut
+                    }
+                    vehicle {
+                        type
+                        patente
+                    }
+                    parcel {
+                        number
+                    }
+                    observation
+                    dateOut
                 }
             }`
         }
@@ -45,6 +59,118 @@ const RecordsList = () => {
             }
         </div>
     );
+}*/
+
+const getHour = dateTime => dateTime ? dateTime.slice(11,16) : '';
+
+const Table = ({ columns, data }) => {
+    const { getTableProps, headers, rows, prepareRow } = useTable({
+        columns,
+        data
+    });
+
+    return (
+        <table {...getTableProps()} className='records-table'>
+            <thead>
+                <tr>
+                    {headers.map(header => (
+                        <th { ...header.getHeaderProps() }>{ header.render('Header') }</th>
+                    ))}
+                </tr>   
+            </thead>
+            <tbody>
+                {rows.map((row, i) => prepareRow(row) || (
+                    <tr {...row.getRowProps()}>
+                        {row.cells.map(cell => {
+                            if(cell.column.id === "dateIn" || cell.column.id === "dateOut") cell.value = getHour(cell.value);
+                            return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                        })}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+const RecordsList = () => {
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'INGRESO',
+                accessor: 'dateIn',
+            },
+            {
+                Header: 'NOMBRE',
+                accessor: 'person.name',
+            },
+            {
+                Header: 'RUT',
+                accessor: 'person.rut',
+            },
+            {
+                Header: 'VEHICULO',
+                accessor: 'vehicle.type',
+            },
+            {
+                Header: 'PATENTE',
+                accessor: 'vehicle.patente',
+            },
+            {
+                Header: 'PARCELA',
+                accessor: 'parcel.number',
+            },
+            {
+                Header: 'OBSERVACION',
+                accessor: 'observation',
+            },
+            {
+                Header: 'SALIDA',
+                accessor: 'dateOut',
+            }
+        ],
+        []
+    );
+
+    const { loading, cacheValue: { data, ...errors } = {} } = useGraphQL({
+        fetchOptionsOverride(options) {
+            options.url = 'http://192.168.0.109:4000/graphql'
+        },
+        operation: {
+            query: `{
+                records(limit: 0) {
+                    dateIn
+                    person {
+                        name
+                        rut
+                    }
+                    vehicle {
+                        type
+                        patente
+                    }
+                    parcel {
+                        number
+                    }
+                    observation
+                    dateOut
+                }
+            }`
+        }
+    });
+
+    return (
+        <div className='RecordsList'>
+            <TitleBar title='Registros' description='Mostrando los ultimos 12 ingresos al condominio'/>
+            {
+                data ? (
+                    <Table columns={columns} data={data.records} />
+                ) : loading ? (
+                    'Loading...'
+                ) : (
+                    <Errors { ...errors }/>
+                )
+            }
+        </div>
+    )
 }
 
 export default RecordsList;
