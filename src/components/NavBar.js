0d618f4b490dd3c905274827598f203458753a58
RainBoard01 <amdmsi4ever@hotmail.com>
2019-09-25 00:23:44 -0300

    Visual Improvements in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import logo from '../media/logo2.svg';
+import profilePhoto from '../media/profilePhoto.jpg';
+
+const profile = {
+    name: 'Ulises',
+    lastName: 'Acuña'
+};
 
 const NavBar = () => {
     return (
         <div className='NavBar'>
-            <img className='logo' src={ logo } alt='logo'/>
+            <button>
+                <img className='logo' src={ logo } alt='logo'/>
+            </button>
+            <button>Tablero</button>
+            <button>Listas</button>
+            <button>Estadisticas</button>
+            <button>
+                <div><img className='profilePhoto' src={ profilePhoto } alt="profilePhoto"/></div>
+                { profile.name }
+            </button>
         </div>
     )
 }
